3ce4831aff9bf57bce8981dd64c56c24da2f17e3
vagrant <vagrant@RES>
2016-04-14 14:50:08 +0000

    Implementation of IInstrument, Flute and Trumpet according to specification

---
+++ Lab00App-build/Lab00App-code/src/main/java/ch/heigvd/res/lab00/Flute.java
@@ -0,0 +1,16 @@
+package ch.heigvd.res.lab00;
+
+public class Flute implements IInstrument{
+
+	public String play(){
+		return "turlututu";
+	}
+	
+	public int getSoundVolume(){
+		return 42;
+	}
+	
+	public String getColor(){
+		return "not golden...";
+	}
+}
